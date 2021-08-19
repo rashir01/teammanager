a2728daf7fa9cd73dec1951f01e9b87a1cdc6c23
Ray Ashir <rayeed.ashir@gmail.com>
2021-08-18 23:17:25 -0700

    added next question logic to determine what questions to ask based on the user selection

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-//get the required inqierer
-//prompt for the manager name 
-//prompt for the id
-//prompt for the email address 
-//prompt for the office number
+
 const inquirer = require('inquirer');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 const Manager = require ('./lib/Manager');
 
 const COMMON_QUESTIONS = [
@@ -31,26 +29,84 @@ const MANAGER_QUESTIONS = [
     message: 'Enter office number',
     name: 'officeNumber'
   }
-]
+];
 
-const employeeObjects = [];
+const ENGINEER_QUESTIONS = [
+  {
+    type: 'input', 
+    message: 'Enter github id', 
+    name: 'gitHubUserName'
+  }
+];
 
-function init() {
-  console.log("Enter Manager Data");
+const INTERN_QUESTIONS = [
+  {
+    type: 'input', 
+    message: 'Enter school name', 
+    name: 'schoolName'
+  }
+];
+
+const MENU_QUESTION = [ 
+  {
+    type: 'list', 
+    message: 'What would you like to do?',
+    name: 'userSelection', 
+    choices: ['Add Engineer', 'Add Intern', 'Exit']
+  }
+];
+
+const teamArray = [];
+let currentEmployeeType = 'Manager';
+
+function processResponse(response) {
+  switch (currentEmployeeType) {
+    case 'Manager':
+      teamArray.push(new Manager(response.employeeName, response.employeeID, response.employeeEmail, response.officeNumber));
+      break;
+    case 'Engineer': 
+      teamArray.push(new Engineer(response.employeeName, response.employeeID,
+        response.employeeEmail, response.gitHubUserName));
+      break;
+    case 'Intern': 
+      teamArray.push(new Intern(response.employeeName, response.employeeID, response.employeeEmail, response.schoolName));
+      break;
+  }
+  determineNextAction(response.userSelection);
+}
+
+function determineNextAction(option) {
+  switch(option.toString()) {
+    case "Add Engineer": 
+      currentEmployeeType = 'Engineer';
+      askQuestions(ENGINEER_QUESTIONS);
+      break;
+    case 'Add Intern':
+      currentEmployeeType = 'Intern';
+      askQuestions(INTERN_QUESTIONS)
+      break;
+    case 'Exit':
+      console.log(`your employee list is ${JSON.stringify(teamArray)}`);
+      console.log("exiting....");
+      break;
+  }
+}
+
+
+function askQuestions(questions) {
+  console.log(`Enter ${currentEmployeeType} data`);
   inquirer
-  .prompt(COMMON_QUESTIONS.concat(MANAGER_QUESTIONS))
+  .prompt(COMMON_QUESTIONS.concat(questions).concat(MENU_QUESTION))
   .then((response) => {
-    console.log(response);
-    console.log(`Name ${response.employeeName} id ${response.employeeID} email ${response.employeeEmail}`);
-    employeeObjects.push(new Manager(response.employeeName, response.employeeID, response.employeeEmail, response.officeNumber));
-    console.log(employeeObjects);
+    processResponse(response);
   });
 }
 
-init();
+
+askQuestions(MANAGER_QUESTIONS);
 /*
-WHEN I start the application
-THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+WHEN I enter the team manager’s name, employee ID, email address, and office number
+THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
 
 
 User Story
@@ -74,8 +130,7 @@ THEN that GitHub profile opens in a new tab
 
 
 
-WHEN I enter the team manager’s name, employee ID, email address, and office number
-THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+
 
 WHEN I select the engineer option
 THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
@@ -86,6 +141,13 @@ THEN I am prompted to enter the intern’s name, ID, email, and school, and I am
 
 WHEN I decide to finish building my team
 THEN I exit the application, and the HTML is generated
+
+
+DONE
+WHEN I start the application
+THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+
+
 */
 
 
